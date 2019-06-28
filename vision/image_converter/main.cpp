41735e14abc7afe1a467dfdf7e01d3e04605faaf
eithwa <mich841012@gmail.com>
2019-06-28 17:15:47 +0800

    shoot strategy edit avoidancd edit web fix

--- vision/image_converter/main.cpp
+++ vision/image_converter/main.cpp
@@ -12,8 +12,8 @@ int main(int argc, char **argv)
     //signal(SIGINT, SigintHandler);
     ros::Rate loop_rate(60); //program speed limit
 
-    Vision cam("/usb_cam/image_raw");
-    //Vision cam("/raspicam_node/image/compressed");
+    //Vision cam("/usb_cam/image_raw");
+    Vision cam("/raspicam_node/image/compressed");
     fflush(stdout); //更新文字緩衝區
 
     while (ros::ok())
